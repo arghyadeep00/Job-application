8faf22c7c3f30467ce75cd59ef46a446c554c05a
arghyadeep00 <arghyadeepshee00@gmail.com>
2026-09-23 16:24:19 +0530

    fix: update CORS configuration to use environment variable for frontend URL

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ conn();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/application", applicationRoutes);
